5d1cf1854f4e67ef831a68c5bffd7cdd2b9f03e6
Jake <jakedaigle3.14@gmail.com>
2025-04-07 15:05:54 +0200

    Add temperature slider

--- isingsim.py
+++ isingsim.py
@@ -32,26 +32,31 @@ def local_energy(grid, row, col):
 
     return new - current
 
-# grid,beta,J = 50,10,.1
-start_loc = (0, 0)  # Full grid offset
-grid_size = 50  # Number of cells on each side of grid
-cell_size = 12  # Side length of cell
+display_local = False
+display_total = False
+start_loc = (50, 0)  # Full grid offset
+grid_size = 15  # Number of cells on each side of grid
+cell_size = 600//grid_size  # Side length of cell
 gap_size = 0  # Space between cells
 MIX_START = True  # Randomized start or not
-COOLDOWN_TIMER = 20  # Clicking cooldown
-beta = 10  # inverse temperature
-J = .1  # Coupling constant
+COOLDOWN_TIMER = 60  # Clicking cooldown
+J = 1  # Coupling constant
 
 pg.init()
 text = pg.font.SysFont("Comic Sans MS", 30)
 screen_size = grid_size * (cell_size + gap_size)
+
+thermo_offset = 20
+thermo_range = (0, 5)
+temp = 1.
+thermo_pos = [25, thermo_offset + (temp/(thermo_range[1] - thermo_range[0])) * (screen_size - 2 * thermo_offset)]
 
 screen = pg.display.set_mode((screen_size, screen_size))
 pg.display.set_caption("Ising Simulation")
 clock = pg.time.Clock()
 running = True
 
-cols = ["blue", "red"]
+cols = ["#2596be", "#8a0000"]
 df = []
 for i in range(grid_size):
     df.append([])
@@ -72,36 +77,49 @@ while running:
         if event.type == pg.QUIT:
             running = False
 
-    #dt = clock.tick(60)/1000
     if cooldown:
         cooldown -= 1
 
-    select = (np.array(pg.mouse.get_pos()) - start_loc) // (cell_size + gap_size)
+    thermo_display = text.render(str(round(temp, 1)), False, "white")
+
+    select = pg.mouse.get_pos()
     mouse_display = text.render(str(select), False, (0, 0, 0))
-    # energy_display = text.render(str(get_sum(df)), False, (0, 0, 0))
+    if display_total:
+        energy_display = text.render(str(get_sum(df)), False, (0, 0, 0))
 
     randx = np.random.randint(grid_size)
     randy = np.random.randint(grid_size)
 
     energy_diff = local_energy(df, randy, randx)
-    local_display = text.render(str(local_energy(df, select[0], select[1])), False, (0, 0, 0))
+    if display_local:
+        local_display = text.render(str(local_energy(df, select[0], select[1])), False, (0, 0, 0))
     if local_energy(df, randy, randx) < 0:
         df[randy][randx]["spin"] = not df[randy][randx]["spin"]
     else:
-        if np.random.random() < exp(-beta * energy_diff):
+        if np.random.random() < exp(-(1/temp) * energy_diff):
             df[randy][randx]["spin"] = not df[randy][randx]["spin"]
 
-    if pg.mouse.get_pressed()[0] and not cooldown:
-        cooldown = COOLDOWN_TIMER
-        df[select[0]][select[1]]["spin"] = not df[select[0]][select[1]]["spin"]
+    if pg.mouse.get_pressed()[0]:
+        if select[0] < start_loc[0]:
+            if select[1] > thermo_offset and select[1] < screen_size - thermo_offset:
+                thermo_pos[1] = select[1]
+                temp = thermo_range[0] + thermo_range[1] * (thermo_pos[1] - thermo_offset)/(screen_size - 2 * thermo_offset)
+        elif not cooldown:
+            grid_select = (np.array(select) - start_loc) // (cell_size + gap_size)
+            cooldown = COOLDOWN_TIMER
+            df[grid_select[0]][grid_select[1]]["spin"] = not df[grid_select[0]][grid_select[1]]["spin"]
 
     screen.fill("black")
     for i, row in enumerate(df):
         for j, cell in enumerate(row):
             pg.draw.rect(screen, cols[int(cell["spin"])], cell["rect"])
+            pg.draw.circle(screen, "white", thermo_pos, 10)
     
-    # screen.blit(energy_display, (0, 0))
-    screen.blit(local_display, (screen_size - 50, 0))
+    screen.blit(thermo_display, (0, 0))
+    if display_total:
+        screen.blit(energy_display, (50, 0))
+    if display_local:
+        screen.blit(local_display, (screen_size - 50, 0))
 
     pg.display.flip()
 
